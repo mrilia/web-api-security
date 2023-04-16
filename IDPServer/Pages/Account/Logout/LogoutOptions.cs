792a1313bb923a3ad50413323df99d4fa44fe001
mrilia <mrilia@DEV-PC>
2023-04-16 14:26:26 +0430

    feat: add ui to IDPServer

---
+++ IDPServer/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace IDPServer.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
